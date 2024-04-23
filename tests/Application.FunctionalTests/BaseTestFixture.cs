e7e29929759658290273f2d988dc3ab1975517f7
Amr.Mostafa <Amr.Mostafa@AMRMOSTAFA>
2024-04-23 17:15:54 +0200

    push Project

---
+++ tests/Application.FunctionalTests/BaseTestFixture.cs
@@ -0,0 +1,14 @@
+﻿namespace CleanArchitecture.Application.FunctionalTests;
+
+using static Testing;
+
+[TestFixture]
+public abstract class BaseTestFixture
+{
+    [SetUp]
+    //Run code before each test
+    public async Task TestSetUp()
+    {
+        await ResetState();
+    }
+}
---
+++ tests/Application.FunctionalTests/TestDatabaseFactory.cs
@@ -0,0 +1,21 @@
+﻿namespace CleanArchitecture.Application.FunctionalTests;
+
+public static class TestDatabaseFactory
+{
+    public static async Task<ITestDatabase> CreateAsync()
+    {
+#if (UseSQLite)
+        var database = new SqliteTestDatabase();
+#else
+#if DEBUG
+        var database = new SqlServerTestDatabase();
+    #else
+        var database = new TestcontainersTestDatabase();
+    #endif
+#endif
+        // Prepare which table need to deleted data
+        await database.InitialiseAsync();
+
+        return database;
+    }
+}
